961e9f4f632aea1cafdd384c8e1372c7891b3005
Daniel Perez <dpere316@fiu.edu>
2022-05-30 18:22:35 -0400

    nonConstructible Change  algo expert

--- src/com/AlgoExpert/Arrays/NonConstructibleChange.java
+++ src/com/AlgoExpert/Arrays/NonConstructibleChange.java
@@ -1,2 +1,29 @@
-package com.AlgoExpert.Arrays;public class NonConstructibleChange {
+package com.AlgoExpert.Arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class NonConstructibleChange {
+
+    // "coins": [5, 7, 1, 1, 2, 3, 22] -> [1,1,2,3,5,7,22] -> index 1
+    // "min" : 20
+    public static int nonConstructibleChange(int[] coins) {
+        // Write your code here.
+        Arrays.sort(coins);
+        int createdCoin = 0;
+        for(int coin : coins){
+            if(coin > createdCoin +1){
+                return createdCoin +1;
+            }
+            createdCoin += coin;
+        }
+        return createdCoin + 1;
+
+    }
+
+    public static void main(String[] args) {
+        nonConstructibleChange(new int[]{6, 4, 5, 1, 1, 8, 9});
+    }
 }
--- src/com/AlgoExpert/Arrays/SortedSquareArray.java
+++ src/com/AlgoExpert/Arrays/SortedSquareArray.java
@@ -10,7 +10,7 @@ public class SortedSquareArray {
         ans = [1,4,9,25,36,64,81]
     * */
     public static int[] sortedSquareArray(int[] array) {
-        // O(nLogn) time || O(n)
+        // O(n Log n) time || O(n)
         int[]ans = new int[array.length];
         for(int i =0; i < array.length; i++) {
             ans[i] = array[i] * array[i];
