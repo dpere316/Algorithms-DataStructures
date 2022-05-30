6848f45e830151eb21fd35eace32b145598ff62e
Daniel Perez <dpere316@fiu.edu>
2022-05-30 18:36:49 -0400

    leetcode isPalindromeNumber

--- src/com/leetcode/Arrays/problems/isPalindromeNumber.java
+++ src/com/leetcode/Arrays/problems/isPalindromeNumber.java
@@ -1,2 +1,19 @@
-package com.leetcode.Arrays.problems;public class isPalindromeNumber {
+package com.leetcode.Arrays.problems;
+
+public class isPalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        String num = String.valueOf(x);
+        char[] nums = num.toCharArray();
+        for(int i = nums.length-1, j = 0; i >= 0; i--){
+            if(nums[i] != num.charAt(j)){
+                return false;
+            }
+            j++;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        isPalindrome(-121);
+    }
 }
